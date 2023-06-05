73f0c095f4e54e8daa0a5466cca22833f7770b02
eli-ve <89606766+eli-ve@users.noreply.github.com>
2023-06-04 21:54:05 -0500

    updated

---
+++ wouldyourather/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+
+    <h1>Would you rather...?</h1>
+
+    <!--
+    no html elements have dashes, so anything with a dash has
+    to be a vue component - helps avoid confusion
+
+    vvv references 'WouldYouRather' component vvv
+     -->
+    <would-you-rather v-bind:question="wyrQuestion"
+    v-bind:answer1="wyrAnswer1"
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-changed="answerChanged"
+    ></would-you-rather>
+
+    <h2>{{ userSelectionMsg }}</h2>
+
+  </div>
+</template>
+
+<script>
+import WouldYouRather from './components/WouldYouRather.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WouldYouRather
+  },
+  data() {
+    return {  // needs to be sent to WouldYouRather component
+      wyrQuestion: 'Would you rather live in a cave or a tree house?',
+      wyrAnswer1: 'cave',
+      wyrAnswer2: 'tree house',
+      userSelectionMsg: ''
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+      // NOTE: this is a template literal (uses backticks, NOT QUOTES)
+      this.userSelectionMsg = `You chose ${choice}!`
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background: darkslateblue;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: darkslateblue;
+  background: lightskyblue;
+  border: 4px darkslateblue dotted;
+}
+</style>
---
+++ wouldyourather/src/components/WouldYouRather.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wyr">
+
+    <h2>Please make your choice!</h2>
+
+    <h3>{{ question }}</h3>
+
+    <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade">
+    <label>{{ answer1 }}</label>
+
+    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade">
+    <label>{{ answer2 }}</label>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  // data sent from parent > child is called 'props' - used locally, it's just 'data'
+  props: {
+    question: String,
+    answer1: String,
+    answer2: String
+  },
+  data() {
+    return {
+      choice: ''
+    }
+  },
+  methods: {
+    // can use console.log to check if method is being called
+    choiceMade() {
+      // can use *any* non-reserved word(s) as event name
+      this.$emit('answer-changed', this.choice)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+.wyr {
+  margin-bottom: 2rem;
+}
+
+</style>
